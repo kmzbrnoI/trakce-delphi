2e46937648966e0aa0d58fe3be240344b71365dc
Jan Horacek <me@apophis.cz>
2020-01-08 21:21:32 +0100

    Add 2020 to license.

--- Trakce.pas
+++ Trakce.pas
@@ -5,7 +5,7 @@
 {
    LICENSE:
 
-   Copyright 2019 Jan Horacek
+   Copyright 2019-2020 Jan Horacek
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
